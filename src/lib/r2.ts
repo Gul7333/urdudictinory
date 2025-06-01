0d7acbb17387674394e7dbcffbc317631eca2522
Gul7333 <g.task.pk@gmail.com>
2025-06-01 15:43:29 +0500

    add

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -1,9 +1,7 @@
 import {
     S3Client,
-    ListBucketsCommand,
-    ListObjectsV2Command,
+   
     GetObjectCommand,
-    PutObjectCommand,
   } from "@aws-sdk/client-s3";
 import { Readable } from "stream";
 const ACCOUNT_ID = process.env.ACCOUNT_ID || ""
@@ -19,74 +17,6 @@ const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID || ""
     },
   });
   
-  // console.log(await S3.send(new ListBucketsCommand({})));
-  // // {
-  // //     '$metadata': {
-  // //     httpStatusCode: 200,
-  // //         requestId: undefined,
-  // //         extendedRequestId: undefined,
-  // //         cfId: undefined,
-  // //         attempts: 1,
-  // //         totalRetryDelay: 0
-  // // },
-  // //     Buckets: [
-  // //     { Name: 'user-uploads', CreationDate: 2022-04-13T21:23:47.102Z },
-  // //     { Name: 'my-bucket-name', CreationDate: 2022-05-07T02:46:49.218Z }
-  // //     ],
-  // //     Owner: {
-  // //         DisplayName: '...',
-  // //         ID: '...'
-  // //     }
-  // // }
-  
-  // console.log(
-  //   await S3.send(new ListObjectsV2Command({ Bucket: "my-bucket-name" })),
-  // );
-  // // {
-  // //     '$metadata': {
-  // //       httpStatusCode: 200,
-  // //       requestId: undefined,
-  // //       extendedRequestId: undefined,
-  // //       cfId: undefined,
-  // //       attempts: 1,
-  // //       totalRetryDelay: 0
-  // //     },
-  // //     CommonPrefixes: undefined,
-  // //     Contents: [
-  // //       {
-  // //         Key: 'cat.png',
-  // //         LastModified: 2022-05-07T02:50:45.616Z,
-  // //         ETag: '"c4da329b38467509049e615c11b0c48a"',
-  // //         ChecksumAlgorithm: undefined,
-  // //         Size: 751832,
-  // //         StorageClass: 'STANDARD',
-  // //         Owner: undefined
-  // //       },
-  // //       {
-  // //         Key: 'todos.txt',
-  // //         LastModified: 2022-05-07T21:37:17.150Z,
-  // //         ETag: '"29d911f495d1ba7cb3a4d7d15e63236a"',
-  // //         ChecksumAlgorithm: undefined,
-  // //         Size: 279,
-  // //         StorageClass: 'STANDARD',
-  // //         Owner: undefined
-  // //       }
-  // //     ],
-  // //     ContinuationToken: undefined,
-  // //     Delimiter: undefined,
-  // //     EncodingType: undefined,
-  // //     IsTruncated: false,
-  // //     KeyCount: 8,
-  // //     MaxKeys: 1000,
-  // //     Name: 'my-bucket-name',
-  // //     NextContinuationToken: undefined,
-  // //     Prefix: undefined,
-  // //     StartAfter: undefined
-  // //   }
-
-
-
-
   // Converts Readable stream to string
 const streamToString = (stream: Readable): Promise<string> =>
     new Promise((resolve, reject) => {
