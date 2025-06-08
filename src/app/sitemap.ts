893bbb07810dabeeef9572fc7425cc0d124a440a
Gul7333 <g.task.pk@gmail.com>
2025-06-08 11:34:44 +0500

    add

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { dictionaryData } from "@/db";
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return dictionaryData
+    .filter((item) => item && item[1])
+    .map((item) => ({
+      url: `https:urduzaban.pk/${encodeURIComponent(item[1])}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    }));
+
+}
--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -1,8 +1,8 @@
 
-import { redirect } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation'
 
 function Word() {
-  return redirect("/word/%D8%A7%D8%A8%D8%A7%D8%AD%D8%AA%DB%8C")
+  return permanentRedirect("/word/%D8%A7%D8%A8%D8%A7%D8%AD%D8%AA%DB%8C", )
 }
 
 export default Word
