340eb77e3bd0a34ec55f12c1d0f6892dbdb21398
Gul7333 <g.task.pk@gmail.com>
2025-06-09 12:05:56 +0500

    add

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,30 +1,30 @@
-// import { getDictionaryData } from "@/lib/dictionory";
-
-// let data: JsonData[];
-
-// if (process.env.DATA_SOURCE === "remote") {
-//   data = await getDictionaryData(); // fetch from API or remote JSON
-// } else {
-//   // Local fallback
-//   data = require("./onlytest.json") as JsonData[];
-// }
-
-// export const dictionaryData: JsonData[] = data;
-
-
 import { getDictionaryData } from "@/lib/dictionory";
 
 let data: JsonData[];
 
 if (process.env.DATA_SOURCE === "remote") {
   data = await getDictionaryData(); // fetch from API or remote JSON
-  
 } else {
-  // // Local fallback using dynamic import for JSON
-  // const localData = await import("./onlytest.json", {
-  //   assert: { type: "json" },
-  // });
-  // data = localData.default;
+  // Local fallback
+  data = require("./onlytest.json") as JsonData[];
 }
 
 export const dictionaryData: JsonData[] = data;
+
+
+// import { getDictionaryData } from "@/lib/dictionory";
+
+// let data: JsonData[];
+
+// if (process.env.DATA_SOURCE === "remote") {
+//   data = await getDictionaryData(); // fetch from API or remote JSON
+  
+// } else {
+//   // Local fallback using dynamic import for JSON
+//   const localData = await import("./onlytest.json", {
+//     assert: { type: "json" },
+//   });
+//   data = localData.default;
+// }
+
+// export const dictionaryData: JsonData[] = data;
