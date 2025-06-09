3490a69c828a396d0ca17eb026fbf9c89ce2babf
Gul7333 <g.task.pk@gmail.com>
2025-06-09 11:58:56 +0500

    add

--- src/db/index.ts
+++ src/db/index.ts
@@ -18,12 +18,13 @@ let data: JsonData[];
 
 if (process.env.DATA_SOURCE === "remote") {
   data = await getDictionaryData(); // fetch from API or remote JSON
+  
 } else {
-  // Local fallback using dynamic import for JSON
-  const localData = await import("./onlytest.json", {
-    assert: { type: "json" },
-  });
-  data = localData.default;
+  // // Local fallback using dynamic import for JSON
+  // const localData = await import("./onlytest.json", {
+  //   assert: { type: "json" },
+  // });
+  // data = localData.default;
 }
 
 export const dictionaryData: JsonData[] = data;
