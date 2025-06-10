0a8b5bed49cdfc0b3dc46924fd9596d6e258bebc
Gul7333 <g.task.pk@gmail.com>
2025-06-10 11:20:16 +0500

    add

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,40 +1,40 @@
-// import { getDictionaryData } from "@/lib/dictionory";
-
-// let data: JsonData[];
-
-// if (process.env.DATA_SOURCE === "remote") {
-//   data = await getDictionaryData(); // fetch from API or remote JSON
-// } else {
-//   // Local fallback
-//   data = require("./onlytest.json") as JsonData[];
-// }
-
-// async function getdb(){
-//   if (process.env.DATA_SOURCE === "remote") {
-//     data = await getDictionaryData(); // fetch from API or remote JSON
-//   } else {
-//     // Local fallback
-//     data = require("./onlytest.json") as JsonData[];
-//   }
-//   return data
-// }
-
-// export const dictionaryData: JsonData[] = await getdb()
-
-
 import { getDictionaryData } from "@/lib/dictionory";
 
 let data: JsonData[];
 
 if (process.env.DATA_SOURCE === "remote") {
   data = await getDictionaryData(); // fetch from API or remote JSON
-  
 } else {
-  // Local fallback using dynamic import for JSON
-  const localData = await import("./onlytest.json", {
-    assert: { type: "json" },
-  });
-  data = localData.default;
+  // Local fallback
+  data = require("./onlytest.json") as JsonData[];
 }
 
-export const dictionaryData: JsonData[] = data;
+async function getdb(){
+  if (process.env.DATA_SOURCE === "remote") {
+    data = await getDictionaryData(); // fetch from API or remote JSON
+  } else {
+    // Local fallback
+    data = require("./onlytest.json") as JsonData[];
+  }
+  return data
+}
+
+export const dictionaryData: JsonData[] = await getdb()
+
+
+// import { getDictionaryData } from "@/lib/dictionory";
+
+// let data: JsonData[];
+
+// if (process.env.DATA_SOURCE === "remote") {
+//   data = await getDictionaryData(); // fetch from API or remote JSON
+  
+// } else {
+//   // Local fallback using dynamic import for JSON
+//   const localData = await import("./onlytest.json", {
+//     assert: { type: "json" },
+//   });
+//   data = localData.default;
+// }
+
+// export const dictionaryData: JsonData[] = data;
